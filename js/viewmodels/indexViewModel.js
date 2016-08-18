46325303e790e5867b89ddbed666b23b7da1afa3
Abhimanyu Pathania <abpindia1944@gmail.com>
2016-08-18 13:34:31 +0530

    FIX animationend binding bug

--- js/viewmodels/indexViewModel.js
+++ js/viewmodels/indexViewModel.js
@@ -7,9 +7,15 @@ define( ["knockout","jquery"], function(ko, $) {
 		var self = this;
 		self.parentRef = self;
 
+		var header = $("#header");
+
 		// Detect animationend event on header and fadeIn content div
-		$("#header").on("animationend webkitAnimationEnd oAnimationEnd", function () {
+		header.on("animationend webkitAnimationEnd oAnimationEnd", function () {
 			$("#content").addClass("visible");
 		});
+
+		// show header when the script has been loaded and the "animationend" event has
+		// been attached
+		header.css("display", "block");
 	};
 });
